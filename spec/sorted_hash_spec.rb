d0af1c36a50f8bc06924d304775db1f067a30629
James Pike <jpike@chilon.net>
2014-09-06 23:22:51 +0100

    Make hash tests more readable.

--- spec/sorted_hash_spec.rb
+++ spec/sorted_hash_spec.rb
@@ -10,9 +10,9 @@ describe RbLovely::SortedHash do
   end
 
   it "can add elements to hash" do
-    expect(@hash[15] = 9).to equal 9
+    expect(@hash[:o] = 9).to equal 9
     expect(@hash.length).to equal 1
-    expect(@hash[19] = 2).to equal 2
+    expect(@hash[:s] = 2).to equal 2
     expect(@hash.length).to equal 2
   end
 
@@ -24,8 +24,8 @@ describe RbLovely::SortedHash do
 
   it "constructs from array" do
     expect { @hash = RbLovely::SortedHash.new("string should be array") }.to raise_error
-    make_hash 9, 5, 4, 5, 14, 1
-    expect(@hash.to_a).to eql [[14, 1], [9, 5], [4, 5]]
+    make_hash :i, 5, :d, 5, :n, 1
+    expect(@hash.to_a).to eql [[:n, 1], [:i, 5], [:d, 5]]
   end
 
   it "has working constructor shortcut" do
@@ -34,28 +34,28 @@ describe RbLovely::SortedHash do
   end
 
   it "iterates in value order according to <=> operator" do
-    make_hash 2, 14, 3, 1, 4, 99
-    expect { |b| @hash.each(&b) }.to yield_successive_args([3, 1], [2, 14], [4, 99])
+    make_hash :b, 14, :c, 1, :d, 99
+    expect { |b| @hash.each(&b) }.to yield_successive_args([:c, 1], [:b, 14], [:d, 99])
   end
 
   it "does not add duplicates" do
-    make_hash 2, 9, 4, 10
+    make_hash :b, 9, :d, 10
     expect(@hash.length).to equal 2
-    @hash[2] = 12
+    @hash[:b] = 12
     expect(@hash.length).to equal 2
   end
 
   it "can access element by key" do
-    make_hash 2, 9, 4, 10
-    expect(@hash[2]).to equal 9
-    expect(@hash[3]).to equal nil
-    expect(@hash[4]).to equal 10
+    make_hash :b, 9, :d, 10
+    expect(@hash[:b]).to equal 9
+    expect(@hash[:c]).to equal nil
+    expect(@hash[:d]).to equal 10
   end
 
   it "can update element by key" do
-    @hash[2] = 9
-    @hash[2] = 14
-    expect(@hash[2]).to equal 14
+    @hash[:b] = 9
+    @hash[:b] = 14
+    expect(@hash[:b]).to equal 14
   end
 
   it "mixes in method from Enumerable" do
@@ -65,10 +65,8 @@ describe RbLovely::SortedHash do
   end
 
   it "has nice #to_s return value" do
-    make_hash 2, 5, 5, 2
-    @hash[2] = 5
-    @hash[5] = 2
-    expect(@hash.to_s).to eql "RbLovely::SortedHash { 5 => 2, 2 => 5 }"
+    make_hash :b, 5, :e, 2
+    expect(@hash.to_s).to eql "RbLovely::SortedHash { :e => 2, :b => 5 }"
   end
 
   it "has working last and first methods" do
@@ -80,46 +78,46 @@ describe RbLovely::SortedHash do
   end
 
   it "has working delete" do
-    make_hash 8, 3, 9, 1, 2, 7
-    expect(@hash.delete 8).to equal 3
-    expect(@hash.delete 19).to equal nil
-    expect(@hash.to_a).to eql [[9, 1], [2,7]]
+    make_hash :h, 3, :i, 1, :b, 7
+    expect(@hash.delete :h).to equal 3
+    expect(@hash.delete :y).to equal nil
+    expect(@hash.to_a).to eql [[:i, 1], [:b,7]]
   end
 
   it "has working pop" do
-    make_hash 5, 1, 3, 2
+    make_hash :e, 1, :c, 2
     expect(@hash.pop).to equal 2
-    expect(@hash.to_a).to eql [[5,1]]
+    expect(@hash.to_a).to eql [[:e,1]]
   end
 
   it "has working shift" do
-    make_hash 5, 1, 3, 2
+    make_hash :e, 1, :c, 2
     expect(@hash.shift).to equal 1
-    expect(@hash.to_a).to eql [[3,2]]
+    expect(@hash.to_a).to eql [[:c,2]]
   end
 
   it "has working include? and has_key?" do
-    make_hash 5, 1, 3, 2
-    expect(@hash.include? 5).to equal true
-    expect(@hash.has_key? 3).to equal true
+    make_hash :e, 1, :c, 2
+    expect(@hash.include? :e).to equal true
+    expect(@hash.has_key? :c).to equal true
     expect(@hash.key? 6).to equal false
   end
 
   it "can use custom sorting function" do
     # compare reverses normal sort order
     @hash = RbLovely::SortedHash.new(compare: proc { |a, b| b <=> a })
-    @hash[5] = 1
-    @hash[3] = 2
-    @hash[10] = 3
-    expect { |b| @hash.each(&b) }.to yield_successive_args([10, 3], [3, 2], [5, 1])
+    @hash[:e] = 1
+    @hash[:c] = 2
+    @hash[:j] = 3
+    expect(@hash.to_a).to eql [[:j, 3], [:c, 2], [:e, 1]]
   end
 
   it "has working #replace" do
-    make_hash 5, 1, 6, 4
-    expect(@hash.replace 5, 9).to equal 1
-    expect(@hash[5]).to equal 9
-    expect(@hash.replace 8, 10).to equal nil
-    expect(@hash[8]).to equal 10
+    make_hash :e, 1, :f, 4
+    expect(@hash.replace :e, 9).to equal 1
+    expect(@hash[:e]).to equal 9
+    expect(@hash.replace :h, 10).to equal nil
+    expect(@hash[:h]).to equal 10
   end
 
   it "can hash string values" do
@@ -130,15 +128,15 @@ describe RbLovely::SortedHash do
   end
 
   it "has working #clear" do
-    make_hash 5, 2, 3, 5
+    make_hash :e, 2, :c, 5
     @hash.clear
     expect(@hash.to_a).to eql []
   end
 
   it "protects values from garbage collector" do
-    @hash[14] = 'val'
+    @hash[:n] = 'val'
     GC.start
-    expect(@hash[14]).to eql 'val'
+    expect(@hash[:n]).to eql 'val'
   end
 
   it "protects keys from garbage collector" do
