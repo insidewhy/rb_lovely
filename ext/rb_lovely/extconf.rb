3999d32636da7a84bdce8c95eed7d48d5e315fd9
James Pike <jpike@chilon.net>
2014-09-06 10:41:33 +0100

    Warn during installation when boost is not available.

--- ext/rb_lovely/extconf.rb
+++ ext/rb_lovely/extconf.rb
@@ -2,6 +2,8 @@ require 'mkmf'
 $CPPFLAGS += ' -std=c++0x'
 with_cflags('-x c++') do
   # needed for SortedHash, optional
-  have_header('boost/multi_index_container.hpp')
+  unless have_header 'boost/multi_index_container.hpp'
+    warn 'Could not find boost, disabling SortedHash'
+  end
 end
 create_makefile('rb_lovely')
